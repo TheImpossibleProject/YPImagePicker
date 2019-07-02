ae4849837035d1c5031ecad39075daaa6819dff2
Gabor Halasz <gabor@dotapp.studio>
2019-07-02 09:37:42 +0200

    - removing bar button tint colour

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -139,11 +139,6 @@ override open func viewDidLoad() {
                 }
             }
         }
-        
-        // If user has not customized the Nav Bar tintColor, then use black.
-        if UINavigationBar.appearance().tintColor == nil {
-            UINavigationBar.appearance().tintColor  = .black
-        }
     }
     
     deinit {
